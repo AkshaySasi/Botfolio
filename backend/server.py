07c630549d7c2e4b581e6a9f3478030f1c878a11
AkshaySasi <akshaysasi12.knr@gmail.com>
2025-12-22 00:14:10 +0530

    backend issues

--- backend/server.py
+++ backend/server.py
@@ -42,7 +42,7 @@ logger = logging.getLogger(__name__)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', 'http://localhost:3000,http://localhost:3001,http://127.0.0.1:3000,http://127.0.0.1:3001').split(','),
+    allow_origins=os.environ.get('CORS_ORIGINS', 'http://localhost:3000,http://localhost:3001,http://127.0.0.1:3000,http://127.0.0.1:3001,https://mybotfolio.vercel.app').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
